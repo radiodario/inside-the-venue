8da89b551e12ab549f90bfcf0013bf41b55ef025
radiodario <dario.villanueva@gmail.com>
2015-04-01 20:55:17 +0100

    load, gif

--- src/app.js
+++ src/app.js
@@ -10,21 +10,35 @@ var soundEngine = require('./soundEngine.js');
 var randomColor = require('./randomColor.js')
 
 
+var pic = document.querySelector('#image');
 var sound = soundEngine();
+
+
+
 
 sound.setup(audio_files);
 
+function setupClick() {
+  pic.addEventListener('click', function() {
+    var idx = (Math.random() * audio_files.length) | 0
+    sound.play(audio_files[idx], 0);
+    randomColor.start();
+    setTimeout(randomColor.stop, 1000);
+  });
+
+}
 
 
 
-var pic = document.querySelector('#image');
+var loadId = setInterval(function() {
 
-pic.addEventListener('click', function() {
-  var idx = (Math.random() * audio_files.length) | 0
-  sound.play(audio_files[idx], 0);
-  randomColor.start();
-  setTimeout(randomColor.stop, 1000);
-});
+  if (sound.ready()) {
+    clearInterval(loadId);
+    pic.classList.remove('loading');
+    setupClick()
+  }
+}, 100);
+
 
 
 
