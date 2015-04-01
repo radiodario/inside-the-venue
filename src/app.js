858b6091911a28035842396325e8067e78a7c60e
radiodario <dario.villanueva@gmail.com>
2015-04-01 22:33:32 +0100

    airhorn

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ var audio_dir = '/audio/'
 
 var audio_files = [
   'audio/GVL2_InsideTheVenue.mp3',
-  'audio/GVL2_YouKnowWhatToDo.mp3'
+  'audio/GVL2_YouKnowWhatToDo.mp3',
+  'audio/airhorn.mp3'
 ];
+
+var durations = [
+  1100,
+  1100,
+  1600
+]
 
 
 var soundEngine = require('./soundEngine.js');
@@ -23,7 +30,7 @@ function setupClick() {
     var idx = (Math.random() * audio_files.length) | 0
     sound.play(audio_files[idx], 0);
     randomColor.start();
-    setTimeout(randomColor.stop, 1000);
+    setTimeout(randomColor.stop, durations[idx]);
     ga('send', 'event', 'interaction', 'click', 'face');
   });
 
